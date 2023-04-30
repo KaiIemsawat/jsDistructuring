1af2b0b69ed2701bed11c44714304f4ce9cae879
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-04-30 13:37:54 -0400

    first JS commit

---
+++ problemTwo.js
@@ -0,0 +1,10 @@
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name); // <-- some error due to no variable 'name
+console.log(otherName);
+
